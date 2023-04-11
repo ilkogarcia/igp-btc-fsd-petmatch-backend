3587dee210f4ab93edb42a201ab27b5fbedd2dca
Ilko García <ilko.garcia@gmail.com>
2023-04-11 18:34:03 +0200

    fix: Removed node_modules folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import express from 'express'
+
 import * as dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 // For testing purpose
-app.get("/", (req, res) => {
- res.send('<h2>Welcome to PetMatch API... It´s Working!</h2>')
+app.get('/', (req, res) => {
+  res.send('<h2>Welcome to PetMatch API... It´s Working!</h2>')
 })
 
 app.listen(PORT, () => {
-    console.log(`PetMatch API is listening on port ${PORT}`)
+  console.log(`🐶 PetMatch API is listening on port ${PORT}`)
 })
