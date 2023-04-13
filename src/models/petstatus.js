f373cb3b767e812fb6836d2527b1ff4ad1b4d57b
Ilko García <ilko.garcia@gmail.com>
2023-04-13 17:49:22 +0200

    model, migration and seed file generated and executed for pet status table

---
+++ src/models/petstatus.js
@@ -0,0 +1,26 @@
+'use strict'
+const { Model } = require('sequelize')
+
+module.exports = (sequelize, DataTypes) => {
+  class PetStatus extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      PetStatus.hasMany(models.Pet, {
+        foreignKey: 'petId'
+      })
+    }
+  }
+  PetStatus.init({
+    statusName: DataTypes.STRING,
+    statusDescription: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'PetStatus'
+  })
+  return PetStatus
+}
