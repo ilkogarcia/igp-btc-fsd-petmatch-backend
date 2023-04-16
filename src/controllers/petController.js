372dbff5f1859cb210b768125877fb9cbb93516b
Ilko García <ilko.garcia@gmail.com>
2023-04-16 10:57:42 +0200

    refactor: refactor of petController and petService modules. Changed implementation of static methods inside a class to a standard functions.

--- src/controllers/petController.js
+++ src/controllers/petController.js
@@ -4,188 +4,212 @@
 */
 const PetService = require('../services/petService')
 
-class PetController {
-  // CRUD: Create a new pet in database
-  static createNewPet = async (req, res) => {
-    const { body } = req
-    // Validate required properties not missing in the body
-    if (
-      !body.name ||
-      !body.specieId ||
-      !body.breedId ||
-      !body.shelterId ||
-      !body.statusId
-    ) {
-      return res.status(400).json({
-        success: false,
-        message: 'One of the mandatory fields are missing or empty in your request.'
-      })
-    }
-    // Create a newPet object to pass it as an argument to our pet service
-    try {
-      const newPet = {
-        specieId: body.specieId,
-        breedId: body.breedId,
-        shelterId: body.shelterId,
-        statusId: body.statusId,
-        gender: body.gender,
-        name: body.name,
-        age: body.age,
-        description: body.description,
-        imageUrl: body.imageUrl,
-        vaccinationStatus: body.vaccinationStatus,
-        spayedNeutered: body.spayedNeutered,
-      }
-      // Pass newPet to pet service in order to create it on the database
-      const createdPet = await PetService.createNewPet(newPet)
-      return res.status(201).json({
-        sucess: true,
-        message: 'Successfully added the new pet to the database.',
-        info: {
-          count: 1,
-        },
-        data: {
-          pet: createdPet
-        }
-      })
-    } catch (error) {
-      return res.status(500).json({
-        sucess: false,
-        message: error.message,
-      })
-    }
+/**
+ * CRUD: Create a new pet in database.
+ * @param {Object} req - An object that includes a body element with data to create a new pet in the database.
+ * @returns {Object} res - An object in JSON format that includes all info from the recently created pet.
+ */
+const createNewPet = async (req, res) => {
+  const { body } = req
+  // Validate required properties not missing in the body
+  if (
+    !body.name ||
+    !body.specieId ||
+    !body.breedId ||
+    !body.shelterId ||
+    !body.statusId
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: 'One of the mandatory fields are missing or empty in your request.'
+    })
   }
-
-  // CRUD: Retrive an existing pet from database
-  static getOnePet = async (req, res) => {
-    const petId = req.params.petId
-    try {
-      const pet = await PetService.getOnePet(petId)
-      if (!pet) {
-        return res.status(404).json({
-          success: false,
-          message: `Pet ID ${petId} not find.`
-        })
+  // Create a newPet object to pass it as an argument to our pet service
+  try {
+    const newPet = {
+      specieId: body.specieId,
+      breedId: body.breedId,
+      shelterId: body.shelterId,
+      statusId: body.statusId,
+      gender: body.gender,
+      name: body.name,
+      age: body.age,
+      description: body.description,
+      imageUrl: body.imageUrl,
+      vaccinationStatus: body.vaccinationStatus,
+      spayedNeutered: body.spayedNeutered,
+    }
+    // Pass newPet to pet service in order to create it on the database
+    const createdPet = await PetService.createNewPet(newPet)
+    return res.status(201).json({
+      sucess: true,
+      message: 'Successfully added the new pet to the database.',
+      info: {
+        count: 1,
+      },
+      data: {
+        pet: createdPet
       }
-      return res.status(201).json({
-        sucess: true,
-        message: 'Pet info recovered successfully!',
-        info: {
-          count: 1,
-        },
-        data: {
-          pet: pet
-        }
-      })
-    } catch (error) {
-      return res.status(500).json({
-        sucess: false,
-        message: error.message,
-      })
-    }
-  }
-
-  // CRUD: Update an existing pet in database
-  static updateOnePet = async (req, res) => {
-    const { petId } = req.params
-    const { body } = req
-    if (!petId) {
-      return res.status(400).json({
-        success: false,
-        message: `Pet ID ${petId} parameter are missing in your request.`
-      })
-    }
-    // Create a newPet object to pass it as an argument to our pet service
-    try {
-      const updatedPet = await PetService.updateOnePet(petId, body)
-      if (!updatedPet) {
-        return res.status(404).json({
-          success: false,
-          message: `Pet ID ${petId} not find.`
-        })
-      }
-      return res.status(201).json({
-        sucess: true,
-        message: `Pet with ID:${petId} updated successfully`,
-        info: {
-          count: 1,
-        },
-        data: {
-          pet: updatedPet
-        }
-      })
-    } catch (error) {
-      return res.status(500).json({
-        sucess: false,
-        message: error.message,
-      })
-    }
-
-  }
-
-  // CRUD: Delete an existing pet in database
-  static deleteOnePet = async (req, res) => {
-    const petId = req.params.petId
-    if (!petId) {
-      return res.status(404).json({
-        success: false,
-        message: `Pet ID ${petId} parameter are missing in your request.`
-      })
-    }
-    try {
-      const deletedPet = await PetService.deleteOnePet(petId)
-      if (!deletedPet) {
-        return res.status(404).json({
-          success: false,
-          message: `Pet ID ${petId} is not finded on database.`
-        })
-      }
-      return res.status(201).json({
-        sucess: true,
-        message: `Pet with ID:${petId} deleted successfully`,
-        info: {
-          count: 1,
-        },
-        data: {
-          pet: deletedPet,
-        }
-      })
-    } catch (error) {
-      return res.status(500).json({
-        sucess: false,
-        message: error.message,
-      })
-    }
-  }
-
-  // Get list of pets from database (accept and handle query parameters)
-  static getAllPets = async (req, res) => {
-    const queryParams = req.query
-    try {
-      const allPets = await PetService.getAllPets(queryParams)
-      if (!allPets) {
-        return res.status(404).json({
-          sucess: false,
-          message: 'There are no registered pets at this time!'
-        })
-      }
-      return res.status(201).json({
-        sucess: true,
-        message: 'Pets info recovered successfully!',
-        info: {
-          count: allPets.length,
-        },
-        data: {
-          pets: allPets
-        }
-      })
-    } catch (error) {
-      return res.status(500).json({
-        sucess: false,
-        message: error.message,
-      })
-    }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      sucess: false,
+      message: error.message,
+    })
   }
 }
 
-module.exports = PetController
+/**
+ * CRUD: Get an existing pet from the database.
+ * @param {Object} req - An object that includes as a parameter the pet Id to be deleted.
+ * @returns {Object} res - An object in JSON format that includes the retrieved pet info.
+ */
+const getOnePet = async (req, res) => {
+  const petId = req.params.petId
+  try {
+    const pet = await PetService.getOnePet(petId)
+    if (!pet) {
+      return res.status(404).json({
+        success: false,
+        message: `Pet ID ${petId} not find.`
+      })
+    }
+    return res.status(201).json({
+      sucess: true,
+      message: 'Pet info recovered successfully!',
+      info: {
+        count: 1,
+      },
+      data: {
+        pet: pet
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      sucess: false,
+      message: error.message,
+    })
+  }
+}
+
+/**
+ * CRUD: Update an existing pet in database by his Id.
+ * @param {Object} req - An object that includes as a parameter the pet Id to be deleted.
+ * @returns {Object} res - An object in JSON format that includes the updated pet info.
+ */
+const updateOnePet = async (req, res) => {
+  const { petId } = req.params
+  const { body } = req
+  if (!petId) {
+    return res.status(400).json({
+      success: false,
+      message: `Pet ID ${petId} parameter are missing in your request.`
+    })
+  }
+  // Create a newPet object to pass it as an argument to our pet service
+  try {
+    const updatedPet = await PetService.updateOnePet(petId, body)
+    if (!updatedPet) {
+      return res.status(404).json({
+        success: false,
+        message: `Pet ID ${petId} not find.`
+      })
+    }
+    return res.status(201).json({
+      sucess: true,
+      message: `Pet with ID:${petId} updated successfully`,
+      info: {
+        count: 1,
+      },
+      data: {
+        pet: updatedPet
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      sucess: false,
+      message: error.message,
+    })
+  }
+
+}
+
+/**
+ * CRUD: Delete an existing pet in the database by his Id.
+ * @param {Object} req - An object that includes as a parameter the pet Id to be deleted.
+ * @returns {Object} res - An object in JSON format that includes the deleted pet info.
+ */
+const deleteOnePet = async (req, res) => {
+  const petId = req.params.petId
+  if (!petId) {
+    return res.status(404).json({
+      success: false,
+      message: `Pet ID ${petId} parameter are missing in your request.`
+    })
+  }
+  try {
+    const deletedPet = await PetService.deleteOnePet(petId)
+    if (!deletedPet) {
+      return res.status(404).json({
+        success: false,
+        message: `Pet ID ${petId} is not finded on database.`
+      })
+    }
+    return res.status(201).json({
+      sucess: true,
+      message: `Pet with ID:${petId} deleted successfully`,
+      info: {
+        count: 1,
+      },
+      data: {
+        pet: deletedPet,
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      sucess: false,
+      message: error.message,
+    })
+  }
+}
+
+/**
+ * Retrieves all pets that match a specific query.
+ * @param {Object} req - An object that includes in query parameters the filter to be applied.
+ * @returns {Object} res - An object in JSON format that includes all pets recovered in an array.
+ */
+const getAllPets = async (req, res) => {
+  const queryParams = req.query
+  try {
+    const allPets = await PetService.getAllPets(queryParams)
+    if (!allPets) {
+      return res.status(404).json({
+        sucess: false,
+        message: 'There are no registered pets at this time!'
+      })
+    }
+    return res.status(201).json({
+      sucess: true,
+      message: 'Pets info recovered successfully!',
+      info: {
+        count: allPets.length,
+      },
+      data: {
+        pets: allPets
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      sucess: false,
+      message: error.message,
+    })
+  }
+}
+
+module.exports = {
+  createNewPet,
+  getOnePet,
+  updateOnePet,
+  deleteOnePet,
+  getAllPets
+}
--- src/services/petService.js
+++ src/services/petService.js
@@ -1,102 +1,105 @@
 /**
-  * Pet - Service layer. The methods on this case only are responsible
-  * for return the created data and not be responsible for sending the
-  * response to the client..
+  * Service layer, responsible for returning reated data and 
+  * not be responsible for sending the response to the client.
 */
 const { Pet } = require('../models/index')
 
-class PetService {
-  /**
-   * Creates a new pet in database.
-   * @param {Object} petData - The data for the pet to be created.
-   * @returns {Object} The created pet data.
-   */
-  static async createNewPet (petData) {
-    try {
-      const pet = await Pet.create(petData)
-      return {
-        success: true,
-        message: `Pet added successfully with id: ${pet.id}`,
-        data: { pet }
-      }
-    } catch (error) {
-      throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
+/**
+ * Creates a new pet in database.
+ * @param {Object} petData - The data for the pet to be created.
+ * @returns {Object} The created pet data.
+ */
+const createNewPet = async (petData) => {
+  try {
+    const pet = await Pet.create(petData)
+    return {
+      success: true,
+      message: `Pet added successfully with id: ${pet.id}`,
+      data: { pet }
     }
-  }
-
-  /**
-   * Retrieves a pet by id.
-   * @param {Number} petId - The id of the pet to retrieve.
-   * @returns {Object} The retrieved pet data.
-   */
-  static async getOnePet (petId) {
-    try {
-      const pet = await Pet.findByPk(petId)
-      if (!pet) {
-        throw new Error(`No pet found with id: ${petId}`)
-      }
-      return pet
-    } catch (error) {
-      throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
-    }
-  }
-
-  /**
-   * Updates a pet by id.
-   * @param {Number} petId - The id of the pet to update.
-   * @param {Object} petData - The data to update for the pet.
-   * @returns {Object} The updated pet data.
-   */
-  static async updateOnePet (petId, petData) {
-    try {
-      const updateResult = await Pet.update(petData, {
-        where: { id: petId }
-      })
-      if (updateResult[0] === 0) {
-        throw new Error(`No pet found with id: ${petId}`)
-      }
-      const pet = await Pet.findByPk(petId)
-      if (!pet) {
-        throw new Error(`No pet found with id: ${petId}`)
-      }
-      return pet
-    } catch (error) {
-      throw new Error(`Something has gone wrong. ${error.message}`)
-    }
-  }
-
-  /**
-   * Deletes a pet by id.
-   * @param {Number} petId - The id of the pet to delete.
-   * @returns {Object} The deleted pet data.
-   */
-  static async deleteOnePet (petId) {
-    try {
-      const pet = await Pet.findByPk(petId)
-      if (!pet) {
-        throw new Error(`No pet found with id: ${petId}`)
-      }
-      await pet.destroy()
-      return pet
-    } catch (error) {
-      throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
-    }
-  }
-
-  /**
-   * Retrieves all pets.
-   * @returns {Array} An array of all pet data.
-   */
-  static async getAllPets () {
-    try {
-      const pets = await Pet.findAll({
-        order: [['id', 'DESC']]
-      })
-      return pets
-    } catch (error) {
-      throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
-    }
+  } catch (error) {
+    throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
   }
 }
 
-module.exports = PetService
+/**
+ * Retrieves a pet by id.
+ * @param {Number} petId - The id of the pet to retrieve.
+ * @returns {Object} The retrieved pet data.
+ */
+const getOnePet = async (petId) => {
+  try {
+    const pet = await Pet.findByPk(petId)
+    if (!pet) {
+      throw new Error(`No pet found with id: ${petId}`)
+    }
+    return pet
+  } catch (error) {
+    throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
+  }
+}
+
+/**
+ * Updates a pet by id.
+ * @param {Number} petId - The id of the pet to update.
+ * @param {Object} petData - The data to update for the pet.
+ * @returns {Object} The updated pet data.
+ */
+const updateOnePet = async (petId, petData) => {
+  try {
+    const updateResult = await Pet.update(petData, {
+      where: { id: petId }
+    })
+    if (updateResult[0] === 0) {
+      throw new Error(`No pet found with id: ${petId}`)
+    }
+    const pet = await Pet.findByPk(petId)
+    if (!pet) {
+      throw new Error(`No pet found with id: ${petId}`)
+    }
+    return pet
+  } catch (error) {
+    throw new Error(`Something has gone wrong. ${error.message}`)
+  }
+}
+
+/**
+ * Deletes a pet by id.
+ * @param {Number} petId - The id of the pet to delete.
+ * @returns {Object} The deleted pet data.
+ */
+const deleteOnePet = async (petId) => {
+  try {
+    const pet = await Pet.findByPk(petId)
+    if (!pet) {
+      throw new Error(`No pet found with id: ${petId}`)
+    }
+    await pet.destroy()
+    return pet
+  } catch (error) {
+    throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
+  }
+}
+
+/**
+ * Retrieves all pets.
+ * @returns {Array} An array of all pet data.
+ */
+const getAllPets = async () => {
+  try {
+    const pets = await Pet.findAll({
+      order: [['id', 'DESC']]
+    })
+    return pets
+  } catch (error) {
+    throw new Error(`Something has gone wrong. Error: ${error?.message || 'Unknown error'}`)
+  }
+}
+
+module.exports = {
+  createNewPet,
+  getOnePet,
+  updateOnePet,
+  deleteOnePet,
+  getAllPets
+}
