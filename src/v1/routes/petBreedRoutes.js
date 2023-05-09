9c33802b83cc14df52a099414207c3c1dbba65b9
Ilko García <ilko.garcia@gmail.com>
2023-05-09 12:38:52 +0200

    feat: removed isAuthenticated and isAuthorized middlewares from the petBreedRoutes GET endpoints

--- src/v1/routes/petBreedRoutes.js
+++ src/v1/routes/petBreedRoutes.js
@@ -2,8 +2,7 @@
  * @module v1/routes/petBreedRoutes
  * @description Pet breed routes
  * @requires express
- * @requires v1/middlewares/isAuthenticated
- * @requires v1/middlewares/isAuthorized
+ * @requires v1/controllers/petBreedController
  */
 
 // Import express and create router
@@ -11,16 +10,16 @@ const express = require('express')
 const router = express.Router()
 
 // Import security middlewares
-const isAuthenticated = require('../../middlewares/isAuthenticated')
-const isAuthorized = require('../../middlewares/isAuthorized')
+// const isAuthenticated = require('../../middlewares/isAuthenticated')
+// const isAuthorized = require('../../middlewares/isAuthorized')
 
 // Import country controllers
 const PetBreedController = require('../../controllers/petBreedController')
 
 // GET /api/v1/pet-breeds/:petBreedId - Get one pet breed by his id
-router.get('/:petBreedId', isAuthenticated, isAuthorized, PetBreedController.getOnePetBreed)
+router.get('/:petBreedId', PetBreedController.getOnePetBreed)
 
 // GET /api/v1/pet-breeds - Get all pet breeds
-router.get('/', isAuthenticated, isAuthorized, PetBreedController.getAllPetBreeds)
+router.get('/', PetBreedController.getAllPetBreeds)
 
 module.exports = router
