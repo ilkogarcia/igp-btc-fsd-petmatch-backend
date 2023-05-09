972be6025cb0c9ba99abfc67c7d25655954e5dc9
Ilko García <ilko.garcia@gmail.com>
2023-05-09 12:30:40 +0200

    feat: removed isAuthenticated and isAuthorized middlewares from the petSpecieRoutes GET endpoints

--- src/v1/routes/petSpecieRoutes.js
+++ src/v1/routes/petSpecieRoutes.js
@@ -2,8 +2,6 @@
  * @module v1/routes/petSpecieRoutes
  * @description Pet specie routes.
  * @requires express
- * @requires v1/middlewares/isAuthenticated
- * @requires v1/middlewares/isAuthorized
  * @requires v1/controllers/petSpecieController
  */
 
@@ -12,16 +10,16 @@ const express = require('express')
 const router = express.Router()
 
 // Import security middlewares
-const isAuthenticated = require('../../middlewares/isAuthenticated')
-const isAuthorized = require('../../middlewares/isAuthorized')
+// const isAuthenticated = require('../../middlewares/isAuthenticated')
+// const isAuthorized = require('../../middlewares/isAuthorized')
 
 // Import country controllers
 const PetSpecieController = require('../../controllers/petSpecieController')
 
 // GET /v1/pet-species/:petSpecieId - Get one pet specie by his id
-router.get('/:petSpecieId', isAuthenticated, isAuthorized, PetSpecieController.getOnePetSpecie)
+router.get('/:petSpecieId', PetSpecieController.getOnePetSpecie)
 
 // GET /v1/pet-species - Get all pet species
-router.get('/', isAuthenticated, isAuthorized, PetSpecieController.getAllPetSpecies)
+router.get('/', PetSpecieController.getAllPetSpecies)
 
 module.exports = router
