1dda23a9ecbe6e56a71b2b0444b472e564d796b7
Ilko García <ilko.garcia@gmail.com>
2023-05-09 13:32:03 +0200

    feat: removed isAuthenticated and isAuthorized middlewares from the petStatusRoutes GET endpoints

--- src/v1/routes/petStatusRoutes.js
+++ src/v1/routes/petStatusRoutes.js
@@ -2,8 +2,6 @@
  * @module v1/routes/petStatusRoutes
  * @description Pet status routes.
  * @requires express
- * @requires v1/middlewares/isAuthenticated
- * @requires v1/middlewares/isAuthorized
  * @requires v1/controllers/petStatusController
  */
 
@@ -12,16 +10,16 @@ const express = require('express')
 const router = express.Router()
 
 // Import security middlewares
-const isAuthenticated = require('../../middlewares/isAuthenticated')
-const isAuthorized = require('../../middlewares/isAuthorized')
+// const isAuthenticated = require('../../middlewares/isAuthenticated')
+// const isAuthorized = require('../../middlewares/isAuthorized')
 
 // Import country controllers
 const PetStatusController = require('../../controllers/petStatusController')
 
 // GET /v1/pet-statuses/:petStatusId - Get one pet status by his id
-router.get('/:petStatusId', isAuthenticated, isAuthorized, PetStatusController.getOnePetStatus)
+router.get('/:petStatusId', PetStatusController.getOnePetStatus)
 
 // GET /v1/pet-statuses - Get all pet statuses
-router.get('/', isAuthenticated, isAuthorized, PetStatusController.getAllPetStatuses)
+router.get('/', PetStatusController.getAllPetStatuses)
 
 module.exports = router
